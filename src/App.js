1b6c43cdb55b9b402cd1443525bd151a9835fb63
Riddhi Kulkarni <riddhikul26@gmail.com>
2023-07-01 23:08:48 +0530

    added the widgets section , basic build done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "./features/counter/userSlice";
 import Login from "./Login";
 import { auth } from "./firebee";
+import Widgets from "./Widgets";
 //import { unstable_createMuiStrictModeTheme } from "@mui/material";
 
 function App() {
@@ -47,7 +48,9 @@ function App() {
 
       {/*  Feed */}
       <Feed />
+
       {/* Widgets */}
+      <Widgets/>
     </div>
   )}
 </div>
---
+++ src/Widgets.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Widgets.css';
+import InfoIcon from '@mui/icons-material/Info';
+
+function Widgets() {
+  const newsArticle = (heading, subtitle) => (
+    <div className='widgets_article'>
+      <div className='widgets_articleleft'></div>
+      <div className='widgets_articleright'>
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className='widgets'>
+      <div className='widgets_header'>
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle('Iron man is back?', 'Top news ~ 9899 readers')}
+      {newsArticle('Iron man is back?', 'Top news ~ 9899 readers')}
+      {newsArticle('Iron man is back?', 'Top news ~ 9899 readers')}
+
+    </div>
+  );
+}
+
+export default Widgets;
